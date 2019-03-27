6c1e0697a08c571d7171d77bdc8ef50d2eaacf54
Shahid Latif <mrshahidlatif@gmail.com>
2019-03-27 18:59:26 +0100

    revise the main caption

--- js/text.js
+++ js/text.js
@@ -5,7 +5,6 @@ function generateText(data) {
 
     // displaying the captions to screen.
     $("#captionPP").html(generatePPCaption());
-    $("#captionSP").html(generateSPCaption());
 
     drawBarChart("#barBlob", Math.round((blobCt / badCt) * 1000) / 10);
     drawBarChart("#barFc", Math.round((decomCt / badCt) * 1000) / 10);
@@ -225,8 +224,8 @@ function bugText() {
 
 function generatePPCaption() {
     // TODO: color metric names
-    var caption = 'The overview of the software quality in terms of <span class="complexityMetric clickable">complexity</span> (<span class="wmc">wmc</span>, <span class="max_cc">max_cc</span>), <span class="couplingMetric clickable">coupling</span> (<span class="cbo">cbo</span>, <span class="ca">ca</span>, <span class="ce">ce</span>), <span class="cohesionMetric clickable">cohesion</span> (<span class="lcom3">lcom3</span>), and <span class="inheritanceMetric clickable">inheritance</span> (<span class="noc">noc</span>, <span class="dit">dit</span>)';
-    // caption += ' The ' + num2word(countClassesHavingBadSmells(classesWithBadSmells)) + ' <span class="BSLegend">Classes</span> contain bad smells';
+    var caption = 'The overview of the software quality in terms of <span class="complexityMetric clickable">complexity</span> (<span class="wmc">wmc</span>, <span class="max_cc">max_cc</span>), <span class="couplingMetric clickable">coupling</span> (<span class="cbo">cbo</span>, <span class="ca">ca</span>, <span class="ce">ce</span>), <span class="cohesionMetric clickable">cohesion</span> (<span class="lcom3">lcom3</span>), and <span class="inheritanceMetric clickable">inheritance</span> (<span class="noc">noc</span>, <span class="dit">dit</span>). ';
+     caption += 'Each line (left, parallel coordinates plot <span class="pcpInfo infoIcon"title=""> &#9432;</span>) and each dot (right, scatterplot <span class="spInfo infoIcon"title=""> &#9432;</span>) represent a class. The <span class="BSLegend">'+ num2word(countClassesHavingBadSmells(classesWithBadSmells)) + ' Classes</span> out of <span class="NoBSLegend"> ' + classCt + 'Classes</span> contain bad smells';
 
     caption += '. ';
 
@@ -365,7 +364,6 @@ function generateClassDescription(className) {
     if(cObj.npm/cObj.loc < factor*(ss.mean(npm)/ss.mean(loc)) && cObj.loc > ss.mean(loc)){
         text += ' Compared with the average metric values of the classes, it has large size (<span class="loc">loc</span>) but less number of public methods (<span class="npm">npm</span>).';
     }
-
     return text;
 }
 
